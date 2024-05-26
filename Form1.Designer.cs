78c81476504315a0c00fc28f1107f98d6575ec8d
BubbyMae <131413098+BubbyMae@users.noreply.github.com>
2024-05-26 21:50:40 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace KenBelvestre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.columnName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnEmail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnContact = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnpost = new System.Windows.Forms.Button();
+            this.displaylist = new System.Windows.Forms.ListBox();
+            this.addTxt = new System.Windows.Forms.TextBox();
+            this.mailTxt = new System.Windows.Forms.TextBox();
+            this.numberTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.getbtn = new System.Windows.Forms.Button();
+            this.downbtn = new System.Windows.Forms.Button();
+            this.delbtn = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.columnName,
+            this.columnAddress,
+            this.columnEmail,
+            this.columnContact});
+            this.dataGridView1.Location = new System.Drawing.Point(429, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(448, 225);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // columnName
+            // 
+            this.columnName.HeaderText = "NAME";
+            this.columnName.Name = "columnName";
+            // 
+            // columnAddress
+            // 
+            this.columnAddress.HeaderText = "ADDRESS";
+            this.columnAddress.Name = "columnAddress";
+            // 
+            // columnEmail
+            // 
+            this.columnEmail.HeaderText = "EMAIL ACCOUNT";
+            this.columnEmail.Name = "columnEmail";
+            // 
+            // columnContact
+            // 
+            this.columnContact.HeaderText = "CONTACT NUMBER";
+            this.columnContact.Name = "columnContact";
+            // 
+            // nameTxt
+            // 
+            this.nameTxt.Location = new System.Drawing.Point(31, 65);
+            this.nameTxt.Name = "nameTxt";
+            this.nameTxt.Size = new System.Drawing.Size(155, 20);
+            this.nameTxt.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "FULL NAME";
+            // 
+            // btnpost
+            // 
+            this.btnpost.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnpost.Location = new System.Drawing.Point(31, 303);
+            this.btnpost.Name = "btnpost";
+            this.btnpost.Size = new System.Drawing.Size(155, 82);
+            this.btnpost.TabIndex = 3;
+            this.btnpost.Text = "POST";
+            this.btnpost.UseVisualStyleBackColor = true;
+            this.btnpost.Click += new System.EventHandler(this.btnpost_Click);
+            // 
+            // displaylist
+            // 
+            this.displaylist.FormattingEnabled = true;
+            this.displaylist.Location = new System.Drawing.Point(460, 378);
+            this.displaylist.Name = "displaylist";
+            this.displaylist.Size = new System.Drawing.Size(240, 186);
+            this.displaylist.TabIndex = 4;
+            // 
+            // addTxt
+            // 
+            this.addTxt.Location = new System.Drawing.Point(31, 129);
+            this.addTxt.Name = "addTxt";
+            this.addTxt.Size = new System.Drawing.Size(155, 20);
+            this.addTxt.TabIndex = 5;
+            // 
+            // mailTxt
+            // 
+            this.mailTxt.Location = new System.Drawing.Point(234, 65);
+            this.mailTxt.Name = "mailTxt";
+            this.mailTxt.Size = new System.Drawing.Size(167, 20);
+            this.mailTxt.TabIndex = 6;
+            // 
+            // numberTxt
+            // 
+            this.numberTxt.Location = new System.Drawing.Point(234, 129);
+            this.numberTxt.Name = "numberTxt";
+            this.numberTxt.Size = new System.Drawing.Size(167, 20);
+            this.numberTxt.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Himalaya", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(231, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 34);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Get the data from the Browser\r\nor retrieves data from the internet";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Himalaya", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 34);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Post the filled textboxes\r\n in the data grid";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(231, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 18);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "CONTACT NUMBER";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(231, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 18);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "EMAIL ACCOUNT";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(28, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 18);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "ADDRESS";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(745, 151);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 16;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("MS Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(735, 367);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 19;
+            // 
+            // getbtn
+            // 
+            this.getbtn.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getbtn.Location = new System.Drawing.Point(234, 303);
+            this.getbtn.Name = "getbtn";
+            this.getbtn.Size = new System.Drawing.Size(167, 82);
+            this.getbtn.TabIndex = 20;
+            this.getbtn.Text = "GET";
+            this.getbtn.UseVisualStyleBackColor = true;
+            this.getbtn.Click += new System.EventHandler(this.getbtn_Click);
+            // 
+            // downbtn
+            // 
+            this.downbtn.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.downbtn.Location = new System.Drawing.Point(234, 482);
+            this.downbtn.Name = "downbtn";
+            this.downbtn.Size = new System.Drawing.Size(167, 82);
+            this.downbtn.TabIndex = 21;
+            this.downbtn.Text = "DOWNLOAD";
+            this.downbtn.UseVisualStyleBackColor = true;
+            // 
+            // delbtn
+            // 
+            this.delbtn.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delbtn.Location = new System.Drawing.Point(31, 482);
+            this.delbtn.Name = "delbtn";
+            this.delbtn.Size = new System.Drawing.Size(155, 82);
+            this.delbtn.TabIndex = 22;
+            this.delbtn.Text = "DELETE";
+            this.delbtn.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Himalaya", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(34, 462);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(140, 17);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Deletes data on the DataGrid";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Himalaya", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(231, 462);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(176, 17);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Download the Data as a DLL format";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Himalaya", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(706, 266);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(141, 34);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "The Data Grid holds the data\r\ninputted on the text boxes";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Himalaya", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(706, 380);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(189, 34);
+            this.label12.TabIndex = 26;
+            this.label12.Text = "The ListBox displays the JSON format \r\nof the clicked row on the grid";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 586);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.delbtn);
+            this.Controls.Add(this.downbtn);
+            this.Controls.Add(this.getbtn);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numberTxt);
+            this.Controls.Add(this.mailTxt);
+            this.Controls.Add(this.addTxt);
+            this.Controls.Add(this.displaylist);
+            this.Controls.Add(this.btnpost);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameTxt);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox nameTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnpost;
+        private System.Windows.Forms.ListBox displaylist;
+        private System.Windows.Forms.TextBox addTxt;
+        private System.Windows.Forms.TextBox mailTxt;
+        private System.Windows.Forms.TextBox numberTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button getbtn;
+        private System.Windows.Forms.Button downbtn;
+        private System.Windows.Forms.Button delbtn;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnAddress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnEmail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnContact;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace KenBelvestre
+{
+    public partial class Form1 : Form
+    {
+        public class PostData
+        {
+            public string Name { get; set; }
+            public string Location { get; set; }
+            public string Mail { get; set; }
+            public long Number { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnpost_Click(object sender, EventArgs e)
+        {
+            // Check and parse the contact number
+            if (!long.TryParse(numberTxt.Text, out long contactNumber))
+            {
+                MessageBox.Show("Please enter a valid number.");
+                return;
+            }
+
+            // Create a new instance of PostData with the collected data
+            PostData newData = new PostData
+            {
+                Name = nameTxt.Text, // Assign TextBox values to PostData properties
+                Location = addTxt.Text,
+                Mail = mailTxt.Text,
+                Number = contactNumber // Use the parsed contact number
+            };
+
+            // Generate URL with query parameters
+            string url = GenerateUrl(newData);
+
+            // Log the URL for debugging
+            Console.WriteLine("Generated URL: " + url);
+            MessageBox.Show("Generated URL: " + url);
+
+            // Open the URL in the default browser
+            OpenUrlInBrowser(url);
+
+            // Add the data to the DataGridView
+            dataGridView1.Rows.Add(newData.Name, newData.Location, newData.Mail, newData.Number);
+        }
+
+        private string GenerateUrl(PostData data)
+        {
+            // Base address of the server
+            string baseUrl = "http://localhost/New%20Folder/post_handler.php";
+
+            // Create a dictionary to hold the query parameters
+            var queryParams = new Dictionary<string, string>
+            {
+                { "Name", data.Name },
+                { "Location", data.Location },
+                { "Mail", data.Mail },
+                { "Number", data.Number.ToString() }
+            };
+
+            // Convert the dictionary to a query string
+            string queryString = string.Join("&", queryParams.Select(kv => $"{WebUtility.UrlEncode(kv.Key)}={WebUtility.UrlEncode(kv.Value)}"));
+
+            // Combine the base URL and the query string
+            return $"{baseUrl}?{queryString}";
+        }
+
+        private void OpenUrlInBrowser(string url)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to open the URL in browser. Error: {ex.Message}");
+            }
+        }
+
+        private async Task<List<PostData>> GetDataAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost/New%20Folder/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("get_handler.php"); // Change this to your actual GET endpoint
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string data = await response.Content.ReadAsStringAsync();
+                        var postDataList = JsonConvert.DeserializeObject<List<PostData>>(data);
+
+                        // Check if the data is valid and not null
+                        if (postDataList != null && postDataList.Count > 0)
+                        {
+                            return postDataList;
+                        }
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    MessageBox.Show($"Request error: {e.Message}");
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show($"Unexpected error: {e.Message}");
+                }
+
+                return new List<PostData>(); // Return an empty list if no data is found or an error occurs
+            }
+        }
+
+        private async void getbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Fetch data from the server
+                List<PostData> data = await GetDataAsync();
+
+                // Check if data is available
+                if (data.Count > 0)
+                {
+                    // Clear existing rows
+                    dataGridView1.Rows.Clear();
+
+                    // Add fetched data to DataGridView
+                    foreach (var item in data)
+                    {
+                        dataGridView1.Rows.Add(item.Name, item.Location, item.Mail, item.Number);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No data available.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
